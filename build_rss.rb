f187264e6a5b64f5d8b1ded404b0748ec99f7d9b
Chris Dzombak <chris@chrisdzombak.net>
2018-08-10 15:51:16 -0400

    refactor: build list of items before feed, and limit to 50 items

--- build_rss.rb
+++ build_rss.rb
@@ -22,6 +22,24 @@ headers.zip(expected_headers) do |actual, expected|
     end
 end
 
+items = rows[0...50].map { |row| 
+    cells = row.css("td")
+    id = cells[0].text.strip
+    req_name = cells[1].text.strip
+    html = cells[2].children.to_html.gsub("<br>", "<br />")
+    req_date = Date.strptime(cells[3].text.strip, "%m/%d/%Y")
+    due_date = Date.strptime(cells[4].text.strip, "%m/%d/%Y")
+    status = cells[5].text.strip
+
+    {
+        :id => id,
+        :req_date => req_date,
+        :title => "FOIA \##{id} by #{req_name}: #{status}",
+        :link => "https://www.a2gov.org/departments/city-clerk/Pages/FOIA-Requests.aspx\##{id}",
+        :html_content => "<b>FOIA \##{id}</b><br /><b>Submitted by:</b> #{req_name}<br /><b>Received:</b> #{req_date.strftime("%B %-d, %Y")}<br /><b>Due:</b> #{due_date.strftime("%B %-d, %Y")}<br /><b>Status:</b> #{status}<br /><br />#{html}"
+    }
+}
+
 # options: atom, 2.0, 0.9
 feed = RSS::Maker.make("2.0") do |maker|
     maker.channel.author = "City of Ann Arbor"
@@ -34,28 +52,16 @@ feed = RSS::Maker.make("2.0") do |maker|
     maker.image.url = "http://www.a2gov.org/publishingimages/color-logo.jpg"
     maker.image.title = "City of Ann Arbor FOIA Requests"
 
-    for row in rows
-        cells = row.css("td")
-        req_id = cells[0].text.strip
-        req_name = cells[1].text.strip
-        req_text = cells[2].children.to_html.gsub("<br>", "<br />")
-        req_date = Date.strptime(cells[3].text.strip, "%m/%d/%Y")
-        due_date = Date.strptime(cells[4].text.strip, "%m/%d/%Y")
-        req_status = cells[5].text.strip
-
-        item_link = "https://www.a2gov.org/departments/city-clerk/Pages/FOIA-Requests.aspx\##{req_id}"
-        item_title = "FOIA \##{req_id} by #{req_name}: #{req_status}"
-        xhtml_content = "<b>FOIA \##{req_id}</b><br><b>Submitted by:</b> #{req_name}<br /><b>Received:</b> #{req_date.strftime("%B %-d, %Y")}<br /><b>Due:</b> #{due_date.strftime("%B %-d, %Y")}<br /><b>Status:</b> #{req_status}<br /><br />#{req_text}"
-
+    for i in items
         maker.items.new_item do |item|
-            item.id = req_id.to_s
-            item.link = item_link
-            item.title = item_title
-            item.summary = xhtml_content
+            item.id = i[:id]
+            item.link = i[:link]
+            item.title = i[:title]
+            item.summary = i[:html_content]
             item.content.type = "xhtml"
-            item.content.xhtml = xhtml_content
-            item.pubDate = req_date.to_s
-            item.updated = req_date.to_s
+            item.content.xhtml = i[:html_content]
+            item.pubDate = i[:req_date].to_s
+            item.updated = i[:req_date].to_s
         end
     end
 end

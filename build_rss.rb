1a60e1867d6781be5ea937453bc0fe68030f8a6c
Chris Dzombak <chris@chrisdzombak.net>
2018-08-09 21:44:47 -0400

    v1 outputting real rss2.0

--- build_rss.rb
+++ build_rss.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'rss'
+require 'Date'
 
 page = Nokogiri::HTML(open("https://www.a2gov.org/departments/city-clerk/Pages/FOIA-Requests.aspx"))   
 rows = page.css("article table")[0].css("tr")
@@ -10,26 +12,51 @@ rows = rows.drop(1)
 
 # sanity check table header; abort if things seem wrong
 if headers.length != 6
-	$stderr.puts("Expected 6 headers in table; got #{headers.length}")
-	exit(2)
+    $stderr.puts("Expected 6 headers in table; got #{headers.length}")
+    exit(2)
 end
 expected_headers = ["ID", "Name", "Request", "Received", "Due Date", "Status"]
 headers.zip(expected_headers) do |actual, expected|
-	if actual != expected
-		$stderr.puts("Expected table header #{expected}; got #{actual}")
-		exit(2)
-	end
+    if actual != expected
+        $stderr.puts("Expected table header #{expected}; got #{actual}")
+        exit(2)
+    end
 end
 
-for row in rows
-	cells = row.css("td").map{ |td| td.text.strip }
-	req_id = cells[0]
-	req_name = cells[1]
-	req_text = cells[2]
-	req_date = cells[3]
-	req_status = cells[5]
+# options: atom, 2.0, 0.9
+feed = RSS::Maker.make("2.0") do |maker|
+    maker.channel.author = "City of Ann Arbor"
+    maker.channel.language = "en"
+    maker.channel.updated = Time.now.to_s
+    maker.channel.link = "https://www.a2gov.org/departments/city-clerk/Pages/FOIA-Requests.aspx"
+    maker.channel.about = "https://www.a2gov.org/departments/city-clerk/Pages/FOIA-Requests.aspx"
+    maker.channel.title = "City of Ann Arbor FOIA Requests"
+    maker.channel.description = "FOIA requests handled by the City of Ann Arbor. This feed does not inclide requests for police records."
+    maker.image.url = "http://www.a2gov.org/publishingimages/color-logo.jpg"
+    maker.image.title = "City of Ann Arbor FOIA Requests"
 
-	puts("FOIA #{req_id} from #{req_name} on #{req_date}: #{req_status}")
-	puts(req_text)
-	puts("")
+    for row in rows
+        cells = row.css("td")
+        req_id = cells[0].text.strip
+        req_name = cells[1].text.strip
+        req_text = cells[2].children.to_html.gsub("<br>", "<br />")
+        req_date = Date.strptime(cells[3].text.strip, "%m/%d/%Y")
+        due_date = Date.strptime(cells[4].text.strip, "%m/%d/%Y")
+        req_status = cells[5].text.strip
+
+        xhtml_content = "<b>FOIA \##{req_id}</b><br><b>Submitted by:</b> #{req_name}<br /><b>Received:</b> #{req_date.strftime("%B %-d, %Y")}<br /><b>Due:</b> #{due_date.strftime("%B %-d, %Y")}<br /><b>Status:</b> #{req_status}<br /><br />#{req_text}"
+
+        maker.items.new_item do |item|
+            item.id = req_id.to_s
+            item.link = "https://www.a2gov.org/departments/city-clerk/Pages/FOIA-Requests.aspx"
+            item.title = "FOIA \##{req_id} from #{req_name}"
+            item.summary = xhtml_content
+            item.content.type = "xhtml"
+            item.content.xhtml = xhtml_content
+            item.pubDate = req_date.to_s
+            item.updated = req_date.to_s
+        end
+    end
 end
+
+puts feed
